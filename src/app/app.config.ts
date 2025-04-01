711d4e446921ae7a04d81d08e35ce97495636b95
Manish Rawat <rawatmani@gmail.com>
2025-04-01 17:08:14 -0400

    add doc

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideRouter, withHashLocation } from '@angular/router';
 
+/*
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+};*/
+export const appConfig = {
+  providers: [
+    provideRouter(routes, withHashLocation()) // Enables hash-based routing
+  ]
 };
