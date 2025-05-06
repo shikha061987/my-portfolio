0721ebdc1dfdf8e4744faeb4b6a24cd435bb9ea3
Manish Rawat <rawatmani@gmail.com>
2025-05-06 10:12:17 -0400

    doc folder delete

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [NgFor,NgIf],
+  imports: [NgFor, NgIf],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -19,31 +19,34 @@ export class ProjectsComponent {
 
   projects = [
     {
-      
+
       name: '1) ShopNest E-Commerce Application',
       description: 'Developed a responsive e-commerce application using Angular 19, integrating CSS, Angular Material, and Bootstrap. Implemented REST server Api and fetched data using behavioral observable.Used Angular Router and HttpClient for a full SPA experience. Implement search products by category features, and show shopping product listing, product add to cart, remove from cart, and details shopping product features. ',
-      link: 'https://github.com/shikha061987/Open-Street-Map',
-      image: 'https://imgur.com/xtUCQoS.png' ,// Add your image URL or path here
+      link: 'https://github.com/shikha061987/ShoppingApp',
+      image: 'https://imgur.com/xtUCQoS.png',// Add your image URL or path here
       isZoomed: false
 
 
     },
     {
-      
+
       name: '2) Weather Project',
       description: 'Designed and developed a responsive standalone Weather App using Angular 17+, showcasing real-time weather data for any city using the OpenWeatherMap API. The app is built using modern Angular features such as standalone components, provideHttpClient(), and functional API-based bootstrapping.',
-      link: 'https://github.com/shikha061987/weather-app',
-      image: 'https://imgur.com/6i5cRvY.png' ,// Add your image URL or path here
+     //https://weather-app-fc399.web.app/
+      //link: 'https://github.com/shikha061987/weather-app',
+      link: 'https://weather-app-fc399.web.app/',
+
+      image: 'https://imgur.com/6i5cRvY.png',// Add your image URL or path here
       isZoomed: false
 
 
     },
     {
-      
+
       name: '3) OpenStreet Map Integration Project',
       description: 'Developed an interactive map application with selected brand location from dropdown list using Angular and Leaflet.js for real-time location visualization.',
       link: 'https://github.com/shikha061987/Open-Street-Map',
-      image: 'https://i.imgur.com/HTIEdoX.png' ,// Add your image URL or path here
+      image: 'https://i.imgur.com/HTIEdoX.png',// Add your image URL or path here
       isZoomed: false
 
 
@@ -51,8 +54,11 @@ export class ProjectsComponent {
     {
       name: '4) Live Data Chart Dashboard Application',
       description: 'Developed a real-time data visualization dashboard using Angular and Chart.js to display dynamic, live-updating charts.',
-      link: 'https://github.com/shikha061987/Angular-Chart-Sample',
-      image: 'https://imgur.com/5fPKVd0.png' ,// Add your image URL or path here
+
+      // https://chart-sample-d8524.web.app/
+      // link: 'https://github.com/shikha061987/Angular-Chart-Sample',
+      link: 'https://chart-sample-d8524.web.app/',
+      image: 'https://imgur.com/5fPKVd0.png',// Add your image URL or path here
       isZoomed: false
 
 
@@ -61,7 +67,7 @@ export class ProjectsComponent {
       name: '5) Grocery List Management Application',
       description: 'Developed a dynamic, user-friendly web application to manage and organize grocery shopping lists. The application allows users to create, update, and delete grocery items. Data is integated with mock apis.This project focused on enhancing user experience through an intuitive interface and employing best practices in Angular development.',
       link: 'https://github.com/shikha061987/grocery-list',
-      image: 'https://i.imgur.com/HJFvhaP.png' ,// Add your image URL or path here
+      image: 'https://i.imgur.com/HJFvhaP.png',// Add your image URL or path here
       isZoomed: false
 
 
@@ -70,7 +76,7 @@ export class ProjectsComponent {
       name: '6) User Login Authentication Application',
       description: 'Developed a user authentication website using Angular, integrating various features such as login, cotact information,about us,students details,books details, and session management.	Implemented form validation and error handling for login to improve user experience and reduce errors.',
       link: 'https://github.com/shikha061987/login-auth-guard',
-      image: 'https://i.imgur.com/e36ewxy.png' ,// Add your image URL or path here
+      image: 'https://i.imgur.com/e36ewxy.png',// Add your image URL or path here
       isZoomed: false
 
 
