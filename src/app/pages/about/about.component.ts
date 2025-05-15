0f349fe4e5b4ec21893b99074714ea0edbaf19a2
Manish Rawat <rawatmani@gmail.com>
2025-05-15 12:28:34 -0400

    Removed doc folder from repository

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,36 +7,36 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
-  profile = 
+  profile =
     {
       name: 'Shikha Rawat ',
-      title:'Front-End Developer | Angular Specialist',
+      title: 'Front-End Developer | Angular Specialist',
       description: 'Angular (v14+), TypeScript, JavaScript, HTML5, CSS3',
-      backendtechnologies:'Node.js, Express.js (basic understanding)',
+      backendtechnologies: 'Node.js, Express.js (basic understanding)',
 
-      uiLibraries:'Angular Material, Bootstrap, Tailwind CSS',
-      stateManagement:'RxJS, NgRx ',
+      uiLibraries: 'Angular Material, Bootstrap, Tailwind CSS',
+      stateManagement: 'RxJS, NgRx ',
       apiIntegration: 'RESTful APIs, JSON, HTTPClientModule, Interceptors, GraphQL',
 
-      testingTool:'Jasmine, Karma (Unit Testing)',
+      testingTool: 'Jasmine, Karma (Unit Testing)',
       developmentTools: 'Visual Studio Code, Chrome DevTools',
-      versionControlCD:'Git, GitHub, GitLab, CI/CD basics (GitHub Actions, Jenkins)',
-      authentication:'JSON Web Token, Auth0, Angular Route AuthGaurds',
-      deploy:'Firebase, Githu',
+      versionControlCD: 'Git, GitHub, GitLab, CI/CD basics (GitHub Actions, Jenkins)',
+      authentication: 'JSON Web Tokens, Auth0, Auth Gaurds',
+      deploy: 'Firebase, Github',
       /*Design & UI/UX:Basic knowledge of visual design, wireframing, prototyping with tools Figma, canva*/
-design:'Basic knowledge of visual design, wireframing, prototyping with tools Figma, canva',
+      design: 'Basic knowledge of visual design, wireframing, prototyping with tools Figma, canva',
 
 
       gender: 'Female',
       nationality: 'Indian',
-      location: 'Cumming, GA',
-      githublink: 'https://github.com/shikha061987', 
+      location: 'Cumming, Georgia',
+      githublink: 'https://github.com/shikha061987',
       portfoliolink: 'https://shikha061987.github.io/my-portfolio',
 
-    LinkedIn: 'https://www.linkedin.com/in/shikha-rawat-33591a307/' 
-  
+      LinkedIn: 'https://www.linkedin.com/in/shikha-rawat-33591a307/'
 
-      
+
+
 
 
     }
